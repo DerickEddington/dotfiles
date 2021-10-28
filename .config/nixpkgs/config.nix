08d782bf8094caad1dbe4b81fb4e86ac62454efb
boss <boss@yuiop>
2021-10-27 23:59:47 -0700

    nixpkgs config outline for allowing "unfree" packages.

--- .config/nixpkgs/config.nix
+++ .config/nixpkgs/config.nix
@@ -8,7 +8,23 @@
 # would be defined in /etc/nixos/my-home-manager-for-all-users.nix so that it
 # applies to all users.
 
+{ pkgs, ... }:
+
+let
+  lib = pkgs.lib;
+in
 {
+  # Note that `NIXPKGS_ALLOW_UNFREE=1 nix-env -qa` can be used to see all
+  # "unfree" packages without allowing permanently.
+
+  # Allow and show all "unfree" packages that are available.
+  # allowUnfree = true;
+
+  # Allow and show only select "unfree" packages.
+  allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    # "${name}"
+  ];
+
   packageOverrides = pkgs: {
     nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
       inherit pkgs;
