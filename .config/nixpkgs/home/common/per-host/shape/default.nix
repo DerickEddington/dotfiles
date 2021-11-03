c0e9c8bb2973502d9f367cad79a664b4056883a7
root <root@shape>
2021-11-02 20:38:55 -0700

    Refactor to have per-host definitions.

---
+++ .config/nixpkgs/home/common/per-host/shape/default.nix
@@ -0,0 +1,11 @@
+# Options common to all users but specific to this particular host machine.
+
+{
+  _module.args.mine = {
+    # This DPI corresponds to my current external monitor which is a Philips 346B.
+    dpi = 110;
+    # Defining services.xserver.dpi (in /etc/nixos/configuration.nix) confused
+    # Firefox and degraded its UI, independently of its other DPI settings below.
+    # dpi = nixpkgs.config.services.xserver.dpi;
+  };
+}
