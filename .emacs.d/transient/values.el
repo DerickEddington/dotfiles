fe9e531281fe825013e10d4899adc98b02a1c335
Derick Eddington <4731128+DerickEddington@users.noreply.github.com>
2022-08-19 12:53:21 -0700

    More Magit config.

--- .emacs.d/transient/values.el
+++ .emacs.d/transient/values.el
@@ -1,2 +1,2 @@
-((magit-log:magit-log-mode "-n256" "--decorate")
+((magit-log:magit-log-mode "-n256" "--topo-order" "--graph" "--color" "--decorate")
  (magit-log:magit-status-mode "-n256" "--decorate"))
