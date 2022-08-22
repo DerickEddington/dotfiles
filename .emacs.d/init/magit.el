ad13825d24634ebd4ec70d2546428d893f6e4855
v <v@yoyo>
2022-08-22 10:28:29 -0700

    Disable magit-reset-quickly and unbind its key.
    It's too dangerous for me when I press keys accidentally.

--- .emacs.d/init/magit.el
+++ .emacs.d/init/magit.el
@@ -1,1 +1,6 @@
 (require 'magit)
+
+(put 'magit-reset-quickly 'disabled t)
+(add-hook 'magit-status-mode-hook
+          #'(lambda ()
+              (define-key magit-status-mode-map (kbd "x") nil)))
