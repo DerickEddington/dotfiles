3d6617b98374138eeb3b4351852a94dd9a60883f
v <v@yoyo>
2022-09-20 14:32:23 -0700

    Optimize TRAMP according to some recommendations of its Info doc.

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -223,6 +223,7 @@
  '(python-shell-interpreter "python3")
  '(racer-complete-insert-argument-placeholders nil)
  '(racer-eldoc-timeout 5)
+ '(remote-file-name-inhibit-locks t)
  '(require-final-newline 'ask)
  '(rust-format-on-save t)
  '(rust-indent-method-chain t)
@@ -264,6 +265,7 @@
  '(tramp-remote-path
    '(tramp-default-remote-path tramp-own-remote-path "/bin" "/usr/bin" "/sbin" "/usr/sbin" "/usr/local/bin" "/usr/local/sbin" "/local/bin" "/local/freeware/bin" "/local/gnu/bin" "/usr/freeware/bin" "/usr/pkg/bin" "/usr/contrib/bin" "/opt/bin" "/opt/sbin" "/opt/local/bin"))
  '(tramp-use-ssh-controlmaster-options nil)
+ '(vc-handled-backends '(SVN Git))
  '(view-read-only t)
  '(visual-line-fringe-indicators '(left-curly-arrow right-curly-arrow))
  '(wdired-allow-to-change-permissions t)
