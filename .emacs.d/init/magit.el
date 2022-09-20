bdc8d03d62c0f10a44542737885506462579b572
Derick Eddington <4731128+DerickEddington@users.noreply.github.com>
2022-09-20 13:32:37 -0700

    Work-around a bug in magit+tramp with remote file-names.

--- .emacs.d/init/magit.el
+++ .emacs.d/init/magit.el
@@ -4,3 +4,10 @@
 (add-hook 'magit-status-mode-hook
           #'(lambda ()
               (define-key magit-status-mode-map (kbd "x") nil)))
+
+(advice-add 'magit-process-git-arguments
+            :filter-return
+            #'(lambda (args)
+                (mapcar
+                 #'(lambda (a) (if (tramp-tramp-file-p a) (file-local-name a) a))
+                 args)))
