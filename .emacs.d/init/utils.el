b6e85eb3d4484f30c29cb14709d3235a4ddb6764
Derick Eddington <4731128+DerickEddington@users.noreply.github.com>
2022-09-20 21:39:03 -0700

    Add my-maybe-follow-link to replace disabled vc-follow-symlinks.

--- .emacs.d/init/utils.el
+++ .emacs.d/init/utils.el
@@ -1,3 +1,7 @@
+(defgroup my nil
+  "My own things.")
+
+
 (defun my-alist-remove (al exclude)
   (map-filter
    (lambda (key val) (not (member key exclude)))
@@ -13,4 +17,54 @@
   (custom-set-variables
    '(vc-handled-backends nil)
    '(vc-ignore-dir-regexp "")
-   '(auto-revert-check-vc-info nil)))
+   '(auto-revert-check-vc-info nil))
+  (add-hook 'find-file-hook #'my-maybe-follow-link))
+
+
+(defcustom my-follow-symlinks vc-follow-symlinks
+  "What to do if visiting a symbolic link to another file.
+
+If this variable is `t', follow the link and visit the real file,
+telling you about it in the echo area.  If it is `ask', ask for
+confirmation whether it should follow the link.  If `nil', the link is
+visited and a warning displayed."
+  :type '(choice (const :tag "Ask for confirmation" ask)
+                 (const :tag "Visit link without following and warn" nil)
+                 (const :tag "Follow link without confirmation" t))
+  :group 'my)
+
+(defun my-follow-link ()
+  (vc-follow-link)
+  (message "Followed sym-link to %s" buffer-file-name))
+
+(defun my-maybe-follow-link ()
+  "Somewhat similar to `vc-refresh-state' but reduced for my purposes."
+  (interactive)
+  (let* ((truename (and buffer-file-truename
+                        (expand-file-name buffer-file-truename)))
+         (is-link (and truename
+                       (not (equal buffer-file-name truename)))))
+    (cond ((not is-link) nil)   ;Nothing to do.
+          ((eq my-follow-symlinks nil)
+           (message "Warning: visiting sym-link without following it."))
+          ((or (not (eq my-follow-symlinks 'ask))
+               ;; Assume we cannot ask, default to yes.
+               noninteractive
+               ;; Copied from server-start.  Seems like there should
+               ;; be a better way to ask "can we get user input?"...
+               (and (daemonp)
+                    (null (cdr (frame-list)))
+                    (eq (selected-frame) terminal-frame))
+               ;; If we already visited this file by following
+               ;; the link, don't ask again if we try to visit
+               ;; it again.  GUD does that, and repeated questions
+               ;; are painful.
+               (get-file-buffer
+                (abbreviate-file-name
+                 (file-chase-links buffer-file-name))))
+           (my-follow-link))
+          (t
+           (if (yes-or-no-p (format "Sym-link to %s; follow? " buffer-file-truename))
+               (my-follow-link)
+             (message "Warning: editing through the sym-link bypasses any version control.")
+             )))))
