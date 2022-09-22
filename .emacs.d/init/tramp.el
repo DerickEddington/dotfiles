fb912f171161f19b635d6bb18ea70105af82c3fa
Derick Eddington <4731128+DerickEddington@users.noreply.github.com>
2022-09-22 10:18:05 -0700

    Change tramp-own-remote-path to be first, for Emacs.
    Also, set tramp-remote-path such that its prior value is used.

--- .emacs.d/init/tramp.el
+++ .emacs.d/init/tramp.el
@@ -1,6 +1,7 @@
 (require 'custom)
 (require 'tramp)
 
-;; Per Tramp Info doc section 7 Frequently Asked Questions:
 (custom-set-variables
+ '(tramp-remote-path (cons 'tramp-own-remote-path tramp-remote-path))
+ ;; Per Tramp Info doc section 7 Frequently Asked Questions:
  '(debug-ignored-errors (cons 'remote-file-error debug-ignored-errors)))
