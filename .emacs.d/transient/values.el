2feb143967986a58ff39de68315b41c84a1cdd3f
Derick Eddington <4731128+DerickEddington@users.noreply.github.com>
2023-01-12 13:34:58 -0800

    Save magit-diff custom defaults.

--- .emacs.d/transient/values.el
+++ .emacs.d/transient/values.el
@@ -1,2 +1,3 @@
-((magit-log:magit-log-mode "-n256" "--topo-order" "--graph" "--color" "--decorate")
+((magit-diff:magit-diff-mode "--ignore-all-space" "-M" "-C" "--no-ext-diff" "--stat")
+ (magit-log:magit-log-mode "-n256" "--topo-order" "--graph" "--color" "--decorate")
  (magit-log:magit-status-mode "-n256" "--decorate"))
