29ca5e7c0ea7cd8410a597a8884ff65fd3545b96
Derick Eddington <4731128+DerickEddington@users.noreply.github.com>
2023-06-16 16:46:02 -0700

    Work-around a bug in magit+tramp where some git commands hung.

--- .emacs.d/init/tramp.el
+++ .emacs.d/init/tramp.el
@@ -2,3 +2,18 @@
 (require 'tramp)
 (require 'vagrant-tramp nil t)
 (require 'docker-tramp nil t)
+
+
+(defun my--tramp-send-command--workaround-stty-icanon-bug (conn-vec orig-command &rest args)
+  "See: https://github.com/magit/magit/issues/4720"
+  (let ((command
+         (if (string= "stty -icrnl -icanon min 1 time 0" orig-command)
+             "stty -icrnl"
+           orig-command)))
+    (append (list conn-vec command) args)))
+
+(defun my--tramp-send-command--workaround-stty-icanon-bug--filter-args (args)
+  (apply #'my--tramp-send-command--workaround-stty-icanon-bug args))
+
+(advice-add 'tramp-send-command :filter-args
+            #'my--tramp-send-command--workaround-stty-icanon-bug--filter-args)
