1b8129047d8fed2e9f76dbf6d4c1b259d773a806
Derick Eddington <4731128+DerickEddington@users.noreply.github.com>
2025-04-25 21:57:32 -0700

    Customize Magit transient menus.

--- .config/emacs/transient/levels.el
+++ .config/emacs/transient/levels.el
@@ -1,2 +1,9 @@
 ((magit-commit
+  (magit-commit-reshelve . 5)
+  (magit-commit:--date . 5)
+  (t . 5))
+ (magit-rebase
+  (magit-reshelve-since . 5)
+  (magit-rebase:--exec . 5)
+  (magit:--gpg-sign . 6)
   (t . 5)))
